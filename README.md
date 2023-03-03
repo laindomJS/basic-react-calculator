ad8d0f6de4a083ead855685062d0fae3193bdfa7
laindomJS <adrijar21@gmail.com>
2023-03-03 14:56:38 -0400

    feat: added tailwindcss

--- README.md
+++ README.md
@@ -1,14 +1,11 @@
 # Basic React  Calculator
-
 A basic calculator builded in React for practice.
-## Tech Stack
 
-- Client: React
+
+## Tech Stack
+- Client: React, TailwindCSS
 - Bundler: Vite
 
 
-
-
 ## Authors
-
 - [laindomJS](https://www.github.com/laindomJS)
--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
-import './App.css';
-
 export const App = () => {
 	return (
-		<div className='App'>
-			<h1>Hello World</h1>
+		<div className='w-full min-h-screen flex justify-center items-center bg-slate-700'>
+			<h1 className='font-bold text-5xl text-white'>Hello World</h1>
 		</div>
 	)
 }
