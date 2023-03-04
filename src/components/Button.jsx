e2f71ff1aa2d52d22c380dc72ebfb7ad8461f9c2
laindomJS <adrijar21@gmail.com>
2023-03-04 18:59:55 -0400

    feat: corrected custom class

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,7 +4,7 @@ import { CalculatorCtx } from '../context/CalculatorContext';
 export const Button = ({ type, value }) => {
 	const { handleClick } = useContext(CalculatorCtx);
 
-	const customClass = value === 'Clear' 
+	const customClass = value === 'AC' 
 	? 'w-full text-lg h-12 rounded-md bg-dark-moderate-cyan font-bold text-white shadow-sm shadow-dark-grayish-orange lg:ml-8'
 	: 'w-12 h-12 rounded-md bg-light-grayish-yellow font-bold text-2xl shadow-sm shadow-dark-grayish-orange lg:w-16 lg:h-16 lg:text-3xl';
 
